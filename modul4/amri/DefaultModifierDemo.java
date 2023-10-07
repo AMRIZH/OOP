cb1c97ebbae497b93dc8dd41cbe2c539bc925170
Amri Zadi <114711141+AMRIZH@users.noreply.github.com>
2023-10-07 17:51:02 +0700

    new package

---
+++ modul4/amri/DefaultModifierDemo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package amri;
+import com.mycompany.modul4.DefaultModifier;
+/**
+ *
+ * @author amriz
+ */
+public class DefaultModifierDemo {
+    public static void main (String [] args) {
+        DefaultModifier Halo = new DefaultModifier();
+        //Halo.jumlah();
+    }
+}
---
+++ modul4/amri/PublicModifierDemo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package amri;
+import com.mycompany.modul4.PublicModifier;
+/**
+ *
+ * @author amriz
+ */
+public class PublicModifierDemo {
+    public static void main (String [] args) {
+        PublicModifier Hihi = new PublicModifier();
+        Hihi.kali();
+        Hihi.dibagi();
+        Hihi.rata();
+        Hihi.kurangi();
+        Hihi.tambah();
+    }
+}
