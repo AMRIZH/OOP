20242623a93e185104f37184efb80ffb02cc9c38
Amri Zadi <114711141+AMRIZH@users.noreply.github.com>
2024-01-11 10:16:48 +0700

    UAS

--- UAS/InterfaceDemo.java
+++ UAS/InterfaceDemo.java
@@ -2,22 +2,22 @@ package UAS;
 
 public class InterfaceDemo implements Read{
   @Override
-    public void write(){
-        System.out.println("Amri zadi Hudaya");
-    }
+  public void write(){
+      System.out.println("Amri Zadi Hudaya");
+  }
     
-    @Override
-    public void reading(){
-        System.out.println("L200224288");
-    }
+  @Override
+  public void reading(){
+      System.out.println("L200224288");
+  }
     
-    public void studying(){
-        System.out.println("UMS");
-    }
-    public static void main(String[] args) {
-        InterfaceDemo id = new InterfaceDemo();
-        id.reading();
-        id.write();
-        id.studying();
+  public void studying(){
+      System.out.println("UMS");
+  }
+  public static void main(String[] args) {
+    InterfaceDemo id = new InterfaceDemo();
+    id.reading();
+    id.write();
+    id.studying();
     }
 }
